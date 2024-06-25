34d004ad3489e8267a5ee257d5da13e9dc57f30d
Filipe Duarte <filipe.velascoduarte@gmail.com>
2024-06-25 11:02:26 -0300

    Fix Pause Menu

--- Projeto Cavaleiro/main.gd
+++ Projeto Cavaleiro/main.gd
@@ -4,26 +4,9 @@ extends Node2D
 @export var game_over_ui_template: PackedScene
 
 
-@onready var menu_pause = %Menu_Pause
-
-var paused = false
-
 func _ready():
 	GameManager.game_over.connect(trigger_game_over)
 
-func _process(_delta):
-	if Input.is_action_just_pressed("Pause"):
-		pause_Menu()
-
-func pause_Menu():
-	if paused:
-		menu_pause.hide()
-		Engine.time_scale = 1
-	else:
-		menu_pause.show()
-		Engine.time_scale = 0
-		
-	paused = !paused
 func trigger_game_over():
 	#Deletar Game UI
 	if game_ui:
