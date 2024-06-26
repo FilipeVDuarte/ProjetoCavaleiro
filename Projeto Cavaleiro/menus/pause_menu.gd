b3d6b6be155a2a544a8b1e054ea27e091587ab44
Filipe Duarte <filipe.velascoduarte@gmail.com>
2024-06-26 17:20:45 -0300

    Update Pause Menu

--- Projeto Cavaleiro/menus/pause_menu.gd
+++ Projeto Cavaleiro/menus/pause_menu.gd
@@ -4,6 +4,17 @@ var _is_paused:bool = false:
 	set = set_paused
 	
 
+func _ready():
+	# Referência ao AnimationPlayer
+	var animation_player = $AnimationPlayer
+
+	# Verificar se a animação atual é "RESET" e tocar "default" se necessário
+	if animation_player.current_animation == "RESET":
+		animation_player.play("default")
+	else:
+		animation_player.play("default")
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Pause"):
 		_is_paused = !_is_paused	
